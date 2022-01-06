bb1e76c1c3c7f1c560a88572de87d499b9448c15
Rayman244 <rayhewitt42@icloud.com>
2022-01-05 20:52:23 -0500

    refactored template into its own function

--- index.js
+++ index.js
@@ -4,107 +4,119 @@ const questions = [
     {
         type:'input',
         name:'userName',
-        message:'Whats the name of your github username?'
+        message:'Whats the name of your github username?',
+        default: 'Username'
     },
     {   
         type:'input',
         name:'email',
-        message:'Whats the name of your github email?'
+        message:'Whats the name of your github email?',
+        default: 'Email'
     },
     {
         type:'input',
         name:'projTitle',
-        message:'Whats the name of your project?'
+        message:'Whats the name of your project?',
+        default: 'Title'
     },
     {
         type:'input',
         name:'description',
-        message:'Provide a description of project?'
+        message:'Provide a description of project?',
+        default: 'Description'
     },
     {
         type:'input',
         name:'installation',
-        message:'Whats the installation process?'
+        message:'Whats the installation process?',
+        default: 'Installation'
     },
     {
         type:'input',
         name:'credits',
-        message:'Who would you like to credit?'
+        message:'Who would you like to credit?',
+        default: 'Credits'
     },
     {
         type:'input',
         name:'licenses',
-        message:'What licences are used?'
+        message:'What licences are used?',
+        default: 'Licenses'
     },
     {
         type:'input',
         name:'features',
-        message:'What are some main features?'
+        message:'What are some main features?',
+        default: 'Features'
     },
 
     {
         type:'input',
         name:'tests',
-        message:'Testing results?'
+        message:'Testing results?',
+        default: 'Tests'
     },
 
 
   ]
-
-inquirer
-  .prompt(questions)
-  .then((answers) => { 
-    //   answers.userName,answers.email,answers.projTitle,answers.description,answers.installation,answers.credits,answers.licenses,answers.features,answers.tests
-    
-    const newFile = `${answers.projTitle}-README.md`;
-    const readMeFile = `# ${answers.projTitle}
+  const templateGenerator = (answers) =>{
+      return `# ${answers.projTitle}
 
 ## Description
  ${answers.description}
-       
+             
 ## Table of Contents
-    
+          
  - [Installation](#installation)
  - [Usage](#usage)
  - [Credits](#credits)
  - [License](#license)
  - [Questions](#quesions)
-
+      
 ## Installation
  ${answers.installation} 
-
+      
 ## Usage
  Provide instructions and examples for use. Include screenshots as needed.
  To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-         
-  ![alt text](assets/images/screenshot.png)
-          
+        '''       
+        ![alt text](assets/images/screenshot.png)
+        '''    
+
 ## Credits
  ${answers.credits}
-
+      
 ## License
  ${answers.licenses}
-      
+            
 ## Badges
  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
- ## Features
-       
-
+      
+## Features
+ ${answers.features}     
+      
 ## How to Contribute
  https://github.com/${answers.userName}/
-
-## Tests
       
-
-## Questions
+## Tests
+ ${answers.tests}     
+      
+ ## Questions
  email me at [Email](mailto:${answers.email})
-      `
-      fs.appendFile(newFile,readMeFile,(err) =>
+ 
+ `
+  }
+inquirer
+  .prompt(questions)
+  .then((answers) => { 
+    
+    const newFile = `${answers.projTitle}-README.md`;
+    
+      fs.appendFile(newFile,templateGenerator(answers),(err) =>
       err ? console.error(`Error appending to file \n ${err}`) : console.log('Successfully created README file. Just copy file into project'))
     
-    // 
+    
   })
   .catch((error) => {
     if (error.isTtyError) {
---
+++ test-README.md
@@ -0,0 +1,46 @@
+# test
+
+## Description
+ test
+             
+## Table of Contents
+          
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Credits](#credits)
+ - [License](#license)
+ - [Questions](#quesions)
+      
+## Installation
+ test 
+      
+## Usage
+ Provide instructions and examples for use. Include screenshots as needed.
+ To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+        '''       
+        ![alt text](assets/images/screenshot.png)
+        '''    
+
+## Credits
+ test
+      
+## License
+ test
+            
+## Badges
+ ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+ Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+      
+## Features
+ test     
+      
+## How to Contribute
+ https://github.com/test/
+      
+## Tests
+ test     
+      
+ ## Questions
+ email me at [Email](mailto:test)
+ 
+ 
